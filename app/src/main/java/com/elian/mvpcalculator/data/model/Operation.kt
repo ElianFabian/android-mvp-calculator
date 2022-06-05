9f28882431b1a759df776271fd36fc1398eb083d
Elián <elian.fabian.el.belaizi@gmail.com>
2022-06-05 14:58:09 +0200

    Update Operation.kt

--- app/src/main/java/com/elian/mvpcalculator/data/model/Operation.kt
+++ app/src/main/java/com/elian/mvpcalculator/data/model/Operation.kt
@@ -1,34 +1,35 @@
 package com.elian.mvpcalculator.data.model
 
+import com.elian.mvpcalculator.data.model.OperationError.*
+import com.elian.mvpcalculator.data.model.OperationType.*
+
 data class Operation(val type: OperationType)
 {
     constructor(number1: String, number2: String, type: OperationType) : this(type)
     {
         if (number1.isEmpty())
         {
-            error = OperationError.NUMBER1_IS_NAN
+            error = NUMBER1_IS_NAN
         }
         else this.number1 = number1.toFloat()
 
         if (number2.isEmpty())
         {
-            error = if (error == OperationError.NUMBER1_IS_NAN) OperationError.NUMBERS_ARE_NAN
-            else OperationError.NUMBER2_IS_NAN
+            error = if (error == NUMBER1_IS_NAN) NUMBERS_ARE_NAN
+            else NUMBER2_IS_NAN
         }
         else this.number2 = number2.toFloat()
 
         // The NUMBER2_IS_NAN error has a greater priority than DIVISION_BY_ZERO error
-        if (type == OperationType.DIVISION && this.number2 == 0f &&
-            error != OperationError.NUMBER1_IS_NAN
-        )
+        if (type == DIVISION && this.number2 == 0f && error != NUMBER1_IS_NAN)
         {
-            error = OperationError.DIVISION_BY_ZERO
+            error = DIVISION_BY_ZERO
         }
     }
 
     var id = 0
 
-    var error = OperationError.NO_ERROR
+    var error = NO_ERROR
 
     var number1: Float = Float.NaN
     var number2: Float = Float.NaN
@@ -36,14 +37,14 @@ data class Operation(val type: OperationType)
     val result: Float
         get()
         {
-            if (error != OperationError.NO_ERROR) return Float.NaN
+            if (error != NO_ERROR) return Float.NaN
 
             return when (type)
             {
-                OperationType.ADDITION       -> number1 + number2
-                OperationType.SUBTRACTION    -> number1 - number2
-                OperationType.MULTIPLICATION -> number1 * number2
-                OperationType.DIVISION       -> return if (number2 == 0f) Float.NaN else number1 / number2
+                ADDITION       -> number1 + number2
+                SUBTRACTION    -> number1 - number2
+                MULTIPLICATION -> number1 * number2
+                DIVISION       -> return if (number2 == 0f) Float.NaN else number1 / number2
             }
         }
 
